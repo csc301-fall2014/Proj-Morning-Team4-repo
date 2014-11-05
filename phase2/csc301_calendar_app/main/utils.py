1401c57d3b72ec9c8d56c85c10b6468894ceb133
Sangah <sangah93@gmail.com>
2014-11-05 17:08:38 -0500

    Fixed email validation for school enrol

--- phase2/csc301_calendar_app/main/utils.py
+++ phase2/csc301_calendar_app/main/utils.py
@@ -2,7 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
+''' 
+	Return a response with a template that describes a 401 permission error.
+	Takes a context from the request, and text that completes the following
+	sentence.
+	"Sorry, you don't have permission to __________"
+'''
 def render_permission_denied(context, text):
 	template = loader.get_template('main/permission_denied.html')
 	context['access'] = text
 	return HttpResponse(template.render(context), status=401)
+
+	
