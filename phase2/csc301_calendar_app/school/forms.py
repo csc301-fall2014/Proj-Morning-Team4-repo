6c56069b6c4b4d6a293b37716df19a1713051795
tommy <tommyli113@gmail.com>
2014-11-04 22:41:26 -0500

    updated scheduler and school to import new Student model instead of parent UserProfile

--- phase2/csc301_calendar_app/school/forms.py
+++ phase2/csc301_calendar_app/school/forms.py
@@ -1,10 +1,10 @@
 from school.models import SchoolProfile
 from django import forms
-from main.models import UserProfile
+from main.models import Student
 from django.contrib.auth.models import User
 
 
 class SchoolProfileForm(forms.ModelForm):
     class Meta:
-        model = UserProfile
+        model = Student
         fields = ('school',)
