8a4c4b89ebae2f389cc2eebb8d94cc93fb2823dc
Sangah <sangah93@gmail.com>
2014-11-04 19:51:44 -0500

    edited registration forms to include bootstrap classes

--- phase2/csc301_calendar_app/main/forms.py
+++ phase2/csc301_calendar_app/main/forms.py
@@ -3,14 +3,20 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        widgets = {
+        	'username': forms.TextInput(attrs={'class': 'form-control'}),
+        	'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
-        model = UserProfile
-        fields = ('nickname', 'school')
+    	model = UserProfile
+    	fields = ('nickname', ) # , 'schools' )
+        widgets = {
+        	'nickname': forms.TextInput(attrs={'class': 'form-control'}),
+        }
         
