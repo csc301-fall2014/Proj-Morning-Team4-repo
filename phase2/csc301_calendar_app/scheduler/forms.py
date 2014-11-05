b1650483f1e02697cab783a86699b6dee4bdc4c5
Sangah <sangah93@gmail.com>
2014-11-05 18:09:17 -0500

    cleaned up templates, datetime picker for events

--- phase2/csc301_calendar_app/scheduler/forms.py
+++ phase2/csc301_calendar_app/scheduler/forms.py
@@ -1,16 +1,25 @@
 from scheduler.models import Calendar, Event
 from django.contrib.auth.models import User
 from django import forms
+from bootstrap3_datetime.widgets import DateTimePicker
+
 
 
 class EventForm(forms.ModelForm):
 
-    name = forms.CharField(required=True)
-    description = forms.CharField(required=False)
-    start = forms.DateTimeField(widget=forms.SplitDateTimeWidget(), required=True)
-    end = forms.DateTimeField(widget=forms.SplitDateTimeWidget(), required=False)
-    location = forms.CharField(required=True)
-
     class Meta:
         model = Event
         fields = ('name', 'description', 'start', 'end', 'location')
+        widgets = {
+        	'name': forms.TextInput(attrs={'class': 'form-control'}),
+        	'description': forms.TextInput(attrs={'class': 'form-control'}),
+        	'start': DateTimePicker(attrs={'class': 'form-control'},options={
+        		'format': "MM/DD/YYYY HH:mm",
+        		}),
+        	'end': DateTimePicker(attrs={'class': 'form-control'},options={
+        		'format': "MM/DD/YYYY HH:mm",
+        		}),
+        	'location': forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
+        
