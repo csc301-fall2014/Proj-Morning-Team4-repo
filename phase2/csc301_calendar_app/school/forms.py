cd56a0a9cf43838122b31418863d77eebd645476
Binuri Walpitagamage <g3tdirty@cdf.toronto.edu>
2014-11-05 06:29:24 -0500

    The user can create a course if can_create_course permission exists

--- phase2/csc301_calendar_app/school/forms.py
+++ phase2/csc301_calendar_app/school/forms.py
@@ -1,4 +1,4 @@
-from school.models import SchoolProfile
+from school.models import SchoolProfile, Course
 from django import forms
 from main.models import Student
 from django.contrib.auth.models import User
@@ -8,3 +8,14 @@ class SchoolProfileForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ('school',)
+
+
+class CourseForm(forms.ModelForm):
+
+    code = forms.CharField(required=True)
+    name = forms.CharField(required=True)
+    description = forms.CharField(required=False)
+
+    class Meta:
+        model = Course
+        fields = ('code', 'name', 'description')
