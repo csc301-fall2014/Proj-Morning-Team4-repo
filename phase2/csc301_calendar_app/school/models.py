779ab95fb0512b91bcf1e224e32807d39735a255
Sangah <sangah93@gmail.com>
2014-11-22 14:35:19 -0500

    Instructor has permission to view courses they're teaching

--- phase2/csc301_calendar_app/school/models.py
+++ phase2/csc301_calendar_app/school/models.py
@@ -26,7 +26,7 @@ class Course(models.Model):
     school = models.ForeignKey(SchoolProfile)
     creator = models.ForeignKey(User)
     #note student admin is optional
-    student_admins = models.ManyToManyField('main.Student', blank=True)
+    student_admins = models.ManyToManyField('main.Student', null=False, blank=True)
 
     def __unicode__(self):
         return self.name
