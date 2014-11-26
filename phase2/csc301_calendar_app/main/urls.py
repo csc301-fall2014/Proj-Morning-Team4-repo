95507a1c7348af3c0994fcca002ece111efe7266
Binuri Walpitagamage <g3tdirty@cdf.toronto.edu>
2014-11-26 13:15:22 -0500

    Delete notifications and get admin notification for students

--- phase2/csc301_calendar_app/main/urls.py
+++ phase2/csc301_calendar_app/main/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^edit-profile/$', views.user_update, name='edit-profile'),
     url(r'^event_notifications/$', views.event_notifications, name='notifications'),
-    url(r'^instructor_admin_requests/$', views.instructor_admin_requests, name='admin requests')
+    url(r'^instructor_admin_requests/$', views.instructor_admin_requests, name='admin requests'),
+    url(r'^student_admin_requests/$', views.student_admin_requests, name='student admin requests'),
+    url(r'^delete_notifications/$', views.delete_notifications, name='delete notifications')
 
 )
