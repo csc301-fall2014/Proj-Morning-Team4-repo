f474e4c53dbf35c5232fc63b46bc378b30a2e934
Binuri Walpitagamage <g3tdirty@cdf.toronto.edu>
2014-11-26 00:37:14 -0500

    Create and update personal course and school events

--- phase2/csc301_calendar_app/notifications/models.py
+++ phase2/csc301_calendar_app/notifications/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class Notification(models.Model):
-    title = models.CharField(max_length=256)
-    message = models.TextField()
-    viewed = models.BooleanField(default=False)
+    # 'event' or 'course_admin'
+    notification_type = models.CharField(max_length=256)
+
+    # The object_id will include the event id if it is an event notification
+    # If the notification is a student admin notification, then will include the
+    # admin id.
+    content_type = models.ForeignKey(ContentType, related_name="subject")
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    # All notifications are about a course, school or a user
+    owner_type = models.ForeignKey(ContentType, related_name="owner")
+    owner_id = models.PositiveIntegerField()
+    owner = GenericForeignKey('owner_type', 'owner_id')
+
+    # The owner of the notification
     user = models.ForeignKey(User)
 
-# Create your models here.
+    date_created = models.DateTimeField(auto_now=True)
